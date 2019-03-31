b9d4df0c66342c451568bc8928804438e08c0a95
CNLHC <2463765697@qq.com>
2019-03-31 23:14:34 +0800

    完成编码工作

--- ch1/frontend/src/index.js
+++ ch1/frontend/src/index.js
@@ -1,6 +1,30 @@
 
-function startLottery(){
-    console.log("start!")
+
+sectorMapping={
+    "谢谢参与":[0,2,5,7],
+    "100元话费":[3,6],
+    "10元话费":[9],
+    "50积分":[1,4,8],
+
+}
+
+function startLottery(onsuccess){
+    const request = new XMLHttpRequest();
+    request.timeout=2500
+    request.responseType="json"
+
+    request.onload=function(e){
+        onsuccess(e.target.response)
+    }
+    request.onerror=function(e){
+        alert('系统错误')
+    }
+    request.ontimeout=function(e){
+        alert('响应超时')
+    }
+
+    request.open("GET","http://intern.bytedance.cnworkshop.xyz:4449/api/turnTable");
+    request.send();
 }
 
 function drawTurnTableStatic(){
@@ -37,7 +61,6 @@ function drawTurnTableStatic(){
     }
     var drawPrizeImg=function(sector,src){
         var prizeImg = new Image()
-        console.log(prizeImgHeightOffset)
         prizeImg.src = src
         prizeImg.addEventListener('load', function() {
             ctx.save()
@@ -53,7 +76,6 @@ function drawTurnTableStatic(){
         var sectorEndRad = 36*(sector+1)*Math.PI/180
         var sectorCord1 = p2c(0.88*offset,sectorStartRad)
         var sectorCord2 = p2c(0.88*offset,sectorEndRad)
-        console.log(sectorCord1,sectorCord2)
         ctx.lineTo(sectorCord1[0],sectorCord1[1])
         ctx.arc(0,0,0.88*offset,sectorStartRad,sectorEndRad)
         ctx.lineTo(0,0)
@@ -70,20 +92,25 @@ function drawTurnTableStatic(){
 }
 
 function drawTurnTableAnimated(startLotteryCallback){
+    var rotateAnimationRunning = false
+    var rotateAnimationLength=3000
+    var successAnimationRunning = false
+    var successAnimationLength=2000
+    var successAjax=false;
+    var response;
+
     var canvas = document.getElementById("turn-table-animation-canvas");
     var ctx = canvas.getContext("2d");
     var cW  = ctx.canvas.width
-    var animationRunning = false;
     var offset=cW/2
     ctx.canvas.height=ctx.canvas.width;
     ctx.translate(offset,offset)
     var sector=0;
-    var animationLength=3000
     var pointerImage= new Image();  
+    var pointerHeight=0.255*cW;
+    var pointerWidth=0.205*cW;
     pointerImage.src = './img/Pointer.png'
     pointerImage.addEventListener('load', function() {
-        var pointerHeight=0.255*cW;
-        var pointerWidth=0.205*cW;
         var pointerHeightOffset = (pointerHeight-pointerWidth)+(pointerWidth/2);
         ctx.drawImage(pointerImage,0-pointerWidth/2,0-pointerHeightOffset,pointerWidth,pointerHeight)
     }, false);
@@ -115,24 +142,70 @@ function drawTurnTableAnimated(startLotteryCallback){
         if(!startTimeCached){
             startTimeStamp=timeStamp
             startTimeCached=true
+            console.log("pre animation start")
         }
         clearCanvas()
-        var progress = (timeStamp-startTimeStamp)/animationLength;
+        var progress = (timeStamp-startTimeStamp)/rotateAnimationLength;
         drawPointer(3600*progress)
         var sector=0
         for (sector;sector<10;sector++)
             drawLEDBand(sector,sector==Math.floor(3600*progress%720/72));
-        if(animationRunning)
-            raf = window.requestAnimationFrame(animationDraw);
-        else
+        if(rotateAnimationRunning)
+            window.requestAnimationFrame(animationDraw);
+        else{
             startTimeCached=false
+            if(successAjax){
+                successAjax=false
+                successAnimationRunning=true
+                window.requestAnimationFrame(successAnimationDraw)
+                window.setTimeout(function(){successAnimationRunning=false},successAnimationLength)
+                console.log("pre animation end")
+            }
+        }
     }
+
+    var pointerAngle;
+    var successAnimationDraw=function(timeStamp){
+        var choose=function(choices) {
+            var index = Math.floor(Math.random() * choices.length);
+            return choices[index];
+        }
+        if(!startTimeCached){
+            startTimeStamp=timeStamp
+            startTimeCached=true
+            var domResult =document.getElementById('footer')
+            domResult.innerHTML = (response.name=="谢谢参与"?"":"恭喜您获得:")+response.name
+            pointerAngle=choose(sectorMapping[response.name])*36
+            console.log("success animation start")
+        }
+        var progress = (timeStamp-startTimeStamp)/successAnimationLength;
+
+        clearCanvas()
+            console.log(pointerAngle)
+        drawPointer(pointerAngle)
+        var sector=0;
+        for (sector;sector<10;sector++)
+            drawLEDBand(sector,Math.floor(progress*5)%2==1);
+
+        if(successAnimationRunning)
+            window.requestAnimationFrame(successAnimationDraw)
+        else{
+
+            console.log("success animation end")
+            startTimeCached=false
+            successAjax=false
+        }
+    }
+
     canvas.addEventListener('click',function(e){
-        console.log(e)
-            if (!animationRunning) {
-            raf = window.requestAnimationFrame(animationDraw)
-            window.setTimeout(function(){animationRunning=false},animationLength)
-            animationRunning = true;
+        if(e.layerX<offset+pointerWidth && e.layerX>offset-pointerWidth && e.layerY<offset+pointerWidth && e.layerY>offset-pointerWidth)
+        if (!rotateAnimationRunning && !successAnimationRunning) {
+            var domResult =document.getElementById('footer')
+            domResult.innerHTML = ""
+            startLottery(function(res){successAjax=true,response=res})
+            window.requestAnimationFrame(animationDraw)
+            window.setTimeout(function(){rotateAnimationRunning=false},rotateAnimationLength)
+            rotateAnimationRunning = true;
         }
     })
     
